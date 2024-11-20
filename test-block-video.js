5017148ae0e1315907d3141c4aa8339b04721116
thanhphobethethoi <101928407+thanhphobethethoi@users.noreply.github.com>
2024-11-20 14:01:47 +0700

    Create test-block-video.js

---
+++ test-block-video.js
@@ -0,0 +1,10 @@
+ function run()
+            {
+                var htmlObj = document.getElementById('show');
+                htmlObj.innerHTML = htmlObj.innerHTML + `
+        <video id="my-video" class="video-js vjs-default-skin" controls preload="auto"  controlsList="nodownload" width="640" height="360" data-setup="{}">
+            <source src="https://archive.org/download/quang-cao-topik-1/quang-cao-topik%20%281%29.ia.mp4" type="video/mp4">
+        </video>
+    `;
+            }
+
