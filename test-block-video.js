d7500861d7ecb4376d92ac069615c9a13dd8b637
thanhphobethethoi <101928407+thanhphobethethoi@users.noreply.github.com>
2024-11-21 10:14:35 +0700

    Update test-block-video.js

--- test-block-video.js
+++ test-block-video.js
@@ -2,7 +2,7 @@ function run()
             {
                 var htmlObj = document.getElementById('show');
                 htmlObj.innerHTML = htmlObj.innerHTML + `
-    <video id="my-video" class="video-js vjs-default-skin" controls preload="auto" controlsList="nodownload" width="640" height="360" data-setup="{}">
+    <video id="my-video" class="video-js vjs-default-skin" controls preload="auto" controlsList="nodownload" width="940" height="660" data-setup="{}">
         <source src="https://archive.org/download/quang-cao-topik-1/quang-cao-topik%20%281%29.mp4" type="video/mp4">
     </video>
 `;
